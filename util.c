c527df4513a45e21fc0db5345e41bd131a57c3c3
Eignar17 <Eignar17@gmail.com>
2017-07-05 19:07:19 -0500

    Update util.c

--- util.c
+++ util.c
@@ -4,7 +4,73 @@
 #include <math.h>
 #include <float.h>
 
+int
+getCurrentDeformation (CompScreen *s)
+{
+    CUBE_SCREEN (s);
 
+    CompPlugin *p = NULL;
+    const char plugin[] = "cubeaddon";
+    p = findActivePlugin (plugin);
+    if (p && p->vTable->getObjectOptions)
+    {
+	CompOption *option;
+	int  nOption;
+	Bool cylinderManualOnly = FALSE;
+	Bool unfoldDeformation = TRUE;
+
+	option = (*p->vTable->getObjectOptions) (p, (CompObject *)s,
+		&nOption);
+	option = compFindOption (option, nOption, "cylinder_manual_only", 0);
+
+	if (option)
+	    if (option->value.b)
+		cylinderManualOnly = option->value.b;
+
+	option = (*p->vTable->getObjectOptions) (p, (CompObject *)s,
+		&nOption);
+	option = compFindOption (option, nOption, "unfold_deformation", 0);
+
+	if (option)
+	    if (option->value.b)
+		unfoldDeformation = option->value.b;
+
+	if (s->hsize * cs->nOutput > 2 && s->desktopWindowCount &&
+	    (cs->rotationState == RotationManual ||
+	    (cs->rotationState == RotationChange &&
+	    !cylinderManualOnly)) &&
+	    (!cs->unfolded || unfoldDeformation))
+	{
+	    option = (*p->vTable->getObjectOptions) (p, (CompObject *)s,
+		      &nOption);
+	    option = compFindOption (option, nOption, "deformation", 0);
+
+	    if (option)
+		return (option->value.i);
+	}
+    }
+    return DeformationNone;
+}
+
+int
+getDeformationMode (CompScreen *s)
+{
+    CompPlugin *p = NULL;
+    const char plugin[] = "cubeaddon";
+    p = findActivePlugin (plugin);
+    if (p && p->vTable->getObjectOptions)
+    {
+	CompOption *option;
+	int  nOption;
+	option = (*p->vTable->getObjectOptions) (p, (CompObject *)s,
+		  &nOption);
+	option = compFindOption (option, nOption, "deformation", 0);
+
+	if (option)
+	    return (option->value.i);
+    }
+    return DeformationNone;
+}
 
 float randf (float x) { //return random number in range [0,x)
 	return rand()/(((double)RAND_MAX + 1) / x);
